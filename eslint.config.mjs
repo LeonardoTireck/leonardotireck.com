0fe6e062e691f786d0b508b9c11eb1ce91d21673
Leonardo Tireck <leotireck@gmail.com>
2026-08-15 20:18:47 -0300

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,12 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  { ignores: ["**/.next/**", "**/out/**"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    settings: { react: { version: "19.2" } },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
 ]);
