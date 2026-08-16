bb8dae78ccf7de9956e35179db3dffec8ef99854
Leonardo Tireck <leotireck@gmail.com>
2026-08-16 16:10:09 -0300

    feat: simple v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 export default function Page() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Welcome to my website!</h1>
-      <p>This will be great, come back in a few days...</p>
-    </>
+    <main className="min-h-screen flex items-center bg-zinc-950 text-zinc-100">
+      <section className="mx-auto w-full max-w-4xl px-6">
+        <h1 className="text-5xl font-bold tracking-tight">Leonardo Tireck</h1>
+        <p className="mt-4 text-xl text-zinc-400">Software Engineer</p>
+        <p className="mt-6 max-w-2xl text-lg leading-8 text-zinc-600">
+          I build reliable software.
+        </p>
+
+        <div className="space-x-4">
+          <a href="https://github.com/LeonardoTireck">GitHub</a>
+          <a href="https://www.linkedin.com/in/leonardo-tireck/?locale=en-US">
+            LinkedIn
+          </a>
+        </div>
+      </section>
+    </main>
   );
 }
